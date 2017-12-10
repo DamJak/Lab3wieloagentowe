5b303f3a1581cf39973e89d3eefc836cb2e74f8e
Damian Jakubowski <dameonwp@gmail.com>
2017-12-10 21:57:36 +0100

    dodanie proby zajecia drugiego widelca

--- src/Filozof.java
+++ src/Filozof.java
@@ -26,51 +26,68 @@ public class Filozof extends Agent
         System.out.println("Dodaje Server Agent");
         if(Fnd_Serv(this,"P","Pierog")!=null)
             Pierog = Fnd_Serv(this, "P", "Pierog")[0].getName();
-	set_widelec();
+        set_widelec();
 
         addBehaviour(new TickerBehaviour( this, 1000)
         {
             protected void onTick()
             {
 
-
-                ACLMessage Rcv;
-                ACLMessage Rqst = new ACLMessage();
-                Rqst.setPerformative(ACLMessage.INFORM);
-                Rqst.addReceiver(Pierog);
-                myAgent.send(Rqst);
-                MessageTemplate m = MessageTemplate.MatchPerformative(ACLMessage.AGREE);
-                MessageTemplate m1 = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
-                MessageTemplate b = MessageTemplate.or(m,m1);
-                Rcv = myAgent.blockingReceive(b);
-                if(Rcv != null)
-                {
-                    if(Rcv.getPerformative() == ACLMessage.AGREE)
-                    {
-                        ACLMessage Sndw1 = new ACLMessage();
-                        Sndw1.setPerformative(ACLMessage.INFORM);
-                        index = Rand(0,1);
-                        Sndw1.addReceiver(wid[index]);
-                        myAgent.send(Sndw1);
-                        MessageTemplate m11 = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
-                        MessageTemplate m12 = MessageTemplate.MatchPerformative(ACLMessage.DISCONFIRM);
-                        MessageTemplate b1 = MessageTemplate.or(m11,m12);
-                        ACLMessage Rcvw1 = myAgent.blockingReceive(b1);
-                        if(Rcvw1 != null)
-                        {
-                            if (Rcvw1.getPerformative() == ACLMessage.CONFIRM)
-                            {
-                                System.out.println("ma widelec");
-                            }
-                            if (Rcvw1.getPerformative() == ACLMessage.DISCONFIRM)
-                            {
-                                System.out.println("nie ma widelca");
+                if(ilewidelcow == 0) {
+                    ACLMessage Rcv;
+                    ACLMessage Rqst = new ACLMessage();
+                    Rqst.setPerformative(ACLMessage.INFORM);
+                    Rqst.addReceiver(Pierog);
+                    myAgent.send(Rqst);
+                    MessageTemplate m = MessageTemplate.MatchPerformative(ACLMessage.AGREE);
+                    MessageTemplate m1 = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+                    MessageTemplate b = MessageTemplate.or(m, m1);
+                    Rcv = myAgent.blockingReceive(b);
+                    if (Rcv != null) {
+                        if (Rcv.getPerformative() == ACLMessage.AGREE) {
+                            ACLMessage Sndw1 = new ACLMessage();
+                            Sndw1.setPerformative(ACLMessage.INFORM);
+                            index = Rand(0, 1);
+                            Sndw1.addReceiver(wid[index]);
+                            myAgent.send(Sndw1);
+                            MessageTemplate m11 = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
+                            MessageTemplate m12 = MessageTemplate.MatchPerformative(ACLMessage.DISCONFIRM);
+                            MessageTemplate b1 = MessageTemplate.or(m11, m12);
+                            ACLMessage Rcvw1 = myAgent.blockingReceive(b1);
+                            if (Rcvw1 != null) {
+                                if (Rcvw1.getPerformative() == ACLMessage.CONFIRM) {
+                                    System.out.println("ma widelec");
+                                    ilewidelcow++;
+                                }
+                                if (Rcvw1.getPerformative() == ACLMessage.DISCONFIRM) {
+                                    System.out.println("nie ma widelca");
+                                }
                             }
                         }
+                        if (Rcv.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+                            System.out.println("Brak pierogow");
+                        }
                     }
-                    if (Rcv.getPerformative() == ACLMessage.ACCEPT_PROPOSAL)
+                }
+                else if(ilewidelcow == 1)
+                {
+                    ACLMessage Sndw2 = new ACLMessage();
+                    Sndw2.setPerformative(ACLMessage.INFORM);
+                    Sndw2.addReceiver(wid[1-index]);
+                    myAgent.send(Sndw2);
+                    MessageTemplate m = MessageTemplate.MatchPerformative(ACLMessage.CONFIRM);
+                    MessageTemplate m1 = MessageTemplate.MatchPerformative(ACLMessage.DISCONFIRM);
+                    MessageTemplate b = MessageTemplate.or(m,m1);
+                    ACLMessage Rcvw2 = myAgent.blockingReceive(b);
+                    if(Rcvw2 != null)
                     {
-                        System.out.println("Brak pierogow");
+                        if (Rcvw2.getPerformative() == ACLMessage.CONFIRM) {
+                            System.out.println("ma widelec");
+                            ilewidelcow++;
+                        }
+                        if (Rcvw2.getPerformative() == ACLMessage.DISCONFIRM) {
+                            System.out.println("nie ma widelca");
+                        }
                     }
                 }
             }
